08b26da0deb42b0911da513e8f236c194371cd52
Petro-64 <pniemkov@gmail.com>
2021-04-14 16:53:31 -0400

    Model added

--- subjects/models.py
+++ subjects/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+
+class Subject(models.Model):
+    subject_text = models.CharField(max_length=30)
+    pub_date = models.DateTimeField('date published')
+    categories_number = models.IntegerField(default=0)
+    is_active = models.BooleanField(default=False)
 # Create your models here.
