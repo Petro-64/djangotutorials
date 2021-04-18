2286203e1fff9dc4a647cd416d95167513ef8e96
Your Name <you@example.com>
2021-04-17 20:35:25 -0400

    Static files are added

--- tutorialsproject/views.py
+++ tutorialsproject/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the main 11 index.")
+    return render(request, 'index.html', {})
 
 
 def about(request):
