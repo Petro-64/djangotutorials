7833d03b0ff8bcde8ed13c4ec861dfb716ec7d85
Petro-64 <pniemkov@gmail.com>
2021-04-20 17:03:18 -0400

    Some admin improvements

--- categories/admin.py
+++ categories/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-
 from .models import Catergory
 
-admin.site.register(Catergory)
+class CatergoryAdmin(admin.ModelAdmin):
+    list_display = ('category_text', 'subject', 'tuitorials_number', 'is_active')
+    list_filter = ('subject', 'tuitorials_number')  
+
+
+admin.site.register(Catergory, CatergoryAdmin)
 # Register your models here.
--- subjects/admin.py
+++ subjects/admin.py
@@ -5,7 +5,6 @@ from .models import Subject
 
 
 class SubjectAdmin(admin.ModelAdmin):
-    #exclude = ('url_friendly_text', )
     list_display = ('subject_text', 'pub_date', 'categories_number', )
     list_filter = ('pub_date', )    
 
