4ce5c5f2c4c7d130e799426ae85507fd1a62a0da
Petro-64 <pniemkov@gmail.com>
2021-04-20 14:58:03 -0400

    Categories foreign key added

--- categories/models.py
+++ categories/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from subjects import models as subject
 
 class Catergory(models.Model):
     category_text = models.CharField(max_length=30)
+    subject = models.ForeignKey(subject.Subject, on_delete=models.CASCADE, default=1)
 
     def __str__(self):
       return self.category_text
