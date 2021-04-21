140442bb52d626bade31cec062ae783d4befc1ea
Petro-64 <pniemkov@gmail.com>
2021-04-21 14:34:32 -0400

    Admin iprovements adding actions

--- subjects/admin.py
+++ subjects/admin.py
@@ -1,13 +1,25 @@
 from django.contrib import admin
 from .models import Subject
 
+# Admin Action Functions
+def make_active(modeladmin, request, queryset):
+    queryset.update(is_active = True)
 
+def make_unactive(modeladmin, request, queryset):
+    queryset.update(is_active = False)
+
+# Action description
+make_active.short_description = "Mark Selected Subjects as Active"
+make_unactive.short_description = "Mark Selected Subjects as Unactive"
 
 
 class SubjectAdmin(admin.ModelAdmin):
-    list_display = ('subject_text', 'pub_date', 'categories_number', )
-    list_filter = ('pub_date', )    
+    list_display = ('subject_text', 'pub_date', 'is_active', 'categories_number', )
+    list_filter = ('pub_date', ) 
+    actions = [make_active, make_unactive]   
 
 
 admin.site.register(Subject, SubjectAdmin)
+
+admin.site.site_header = "Django Tutorials by Petro"
 
