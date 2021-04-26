5fb3d43ef96ed146e1099151380261f22f5c6616
Petro-64 <pniemkov@gmail.com>
2021-04-26 12:10:05 -0400

    Uniqueness added to subj name

--- subjects/models.py
+++ subjects/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Subject(models.Model):
-    subject_text = models.CharField(max_length=30)
+    subject_text = models.CharField(max_length=30, unique=True)
     pub_date = models.DateTimeField(auto_now_add = True)
     categories_number = models.IntegerField(default=0)
     is_active = models.BooleanField(default=False)
